323f4fd2167ffdcd643673bd1c5fb21675d406e6
Billy <alexandernguyenbill@gmail.com>
2024-05-29 14:46:03 +0700

    Make the cart interactive

--- script/amazon.js
+++ script/amazon.js
@@ -58,6 +58,8 @@ data-product-id = ${product.id}>
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML
 
+// function addToCart(productId){}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button => 
     {
         button.addEventListener('click' , () =>  {
@@ -81,7 +83,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button =>
                 }
               )
             }
+
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+              cartQuantity += item.quantity;
+            })
             console.log(cart)
+            console.log(cartQuantity)
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
         })
     }
 ))
